943e3d6881d05d8562f5b5e36f02312081ca7694
mromero <mgr_89@hotmail.com>
2020-08-06 12:43:29 -0300

    Formulario

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import imagen from './cryptomonedas.png';
+import Formulario from './components/Formulario'
 
-const Contenedor =styled.div`
+
+const Contenedor = styled.div`
 max-width:900px;
 margin:0 auto;
 @media (min-width:992px){
   display:grid;
-  grid-template-columns:repeat()(2,1fr);
+  grid-template-columns:repeat(2,1fr);
   column-gap:2rem;
 }
 `;
@@ -17,18 +19,37 @@ max-width:100%;
 margin-top:5rem;
 `;
 
+const Heading = styled.h1`
+font-family:'Bebas Neue', cursive;
+color: #FFF;
+text-align:left;
+font-weight:700;
+font-size:50px;
+margin-bottom:50px;
+margin-top:80px;
+
+&::after{
+  content:'';
+  width:100px;
+  height:6px;
+  background-color:#66a2fe;
+  display:block;
+}
+`;
+
 
 function App() {
   return (
     <Contenedor>
       <div>
-          <Imagen
+        <Imagen
           src={imagen}
           alt='imagen cripto'
-          />
+        />
       </div>
       <div>
-
+        <Heading>Cotiza Criptomonedas al instante</Heading>
+        <Formulario/>
       </div>
     </Contenedor>
   );
